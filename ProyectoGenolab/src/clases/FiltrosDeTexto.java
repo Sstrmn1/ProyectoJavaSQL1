a19701d65f459394e931c688e57c1a6d9501ef1e
Fabio <sstrmn1@gmail.com>
2023-12-13 14:44:51 -0400

    Modificar clase filtros de texto, ahora tambien permite caracteres vacios

--- ProyectoGenolab/src/clases/FiltrosDeTexto.java
+++ ProyectoGenolab/src/clases/FiltrosDeTexto.java
@@ -8,13 +8,13 @@ import javax.swing.text.DocumentFilter;
 
 public class FiltrosDeTexto {
 
-    //applyNumericFilter impide que un JTextField reciba caracteres no numericos
+    // applyNumericFilter permite caracteres vacíos y números en un JTextField
     public static void applyNumericFilter(JTextField textField) {
         ((AbstractDocument) textField.getDocument()).setDocumentFilter(new DocumentFilter() {
             @Override
             public void insertString(FilterBypass fb, int offset, String string, AttributeSet attr)
                     throws BadLocationException {
-                if (string.matches("[0-9]+")) {
+                if (string.isEmpty() || string.matches("[0-9]+")) {
                     super.insertString(fb, offset, string, attr);
                 }
             }
@@ -22,7 +22,7 @@ public class FiltrosDeTexto {
             @Override
             public void replace(FilterBypass fb, int offset, int length, String text, AttributeSet attrs)
                     throws BadLocationException {
-                if (text.matches("[0-9]+")) {
+                if (text.isEmpty() || text.matches("[0-9]+")) {
                     super.replace(fb, offset, length, text, attrs);
                 }
             }
